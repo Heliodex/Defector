2acd08ea52b8f5488902d91ffc6a232c7ecd793f
Lewin Kelly <heli@odex.cf>
2026-08-24 18:43:02 -0400

    Split styling of navigation buttons from normal buttons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,78 +33,105 @@ const isAdmin = $derived(user.then(u => (u ? getIsAdmin() : false)))
 	</script>
 </svelte:head>
 
-<header class="max-w-280 mx-auto flex">
-	<nav>
-		<ul class="list-none p-0 m-0 flex gap-8 py-6">
+<nav class="sm:hidden px-4 py-3">
+	<ul class="list-none m-0 p-0 flex flex-wrap gap-2">
+		<li><a class="navbtn" href="/leaderboard">Leaderboard</a></li>
+		{#if await user}
+			<li><a class="navbtn" href="/home">Home</a></li>
+			<li><a class="navbtn" href="/your-bots">Your bots</a></li>
+			<li><a class="navbtn" href="/submit-bot">Submit bot</a></li>
+			<li><a class="navbtn" href="/submit">Submit hours</a></li>
+			{#if await isAdmin}
+				<li><a class="navbtn" href="/admin">Admin</a></li>
+			{/if}
+		{:else}
+			<li><a class="navbtn" href="/">Landing</a></li>
 			<li>
-				<a class="btn btn-primary" href="/leaderboard">Leaderboard</a>
+				<form {...login}>
+					<button class="navbtn" type="submit">Login</button>
+				</form>
+			</li>
+		{/if}
+		<li><a class="navbtn" href="/guide">Guide</a></li>
+	</ul>
+</nav>
+
+<aside
+	class="sidebar hidden sm:block fixed inset-y-0 left-0 w-40 overflow-y-auto border-r-2 border-neutral-200 bg-white"
+>
+	<nav class="p-4">
+		<ul class="list-none m-0 p-0 flex flex-col gap-4">
+			<li>
+				<a class="navbtn" href="/leaderboard">Leaderboard</a>
 			</li>
 			{#if await user}
-				<li><a class="btn btn-primary" href="/home">Home</a></li>
+				<li><a class="navbtn" href="/home">Home</a></li>
 				<li>
-					<a class="btn btn-primary" href="/your-bots">Your bots</a>
+					<a class="navbtn" href="/your-bots">Your bots</a>
 				</li>
 				<li>
-					<a class="btn btn-primary" href="/submit-bot">Submit bot</a>
+					<a class="navbtn" href="/submit-bot">Submit bot</a>
 				</li>
 				<li>
-					<a class="btn btn-primary" href="/submit">Submit hours</a>
+					<a class="navbtn" href="/submit">Submit hours</a>
 				</li>
 				{#if await isAdmin}
-					<li><a class="btn btn-primary" href="/admin">Admin</a></li>
+					<li><a class="navbtn" href="/admin">Admin</a></li>
 				{/if}
 			{:else}
-				<li><a class="btn btn-primary" href="/">Landing</a></li>
+				<li><a class="navbtn" href="/">Landing</a></li>
 				<li>
-					<form {...login} class="-mt-1.5">
-						<!-- why wrong paddingg ggggg -->
-						<button class="btn btn-primary" type="submit">
-							Login
-						</button>
+					<form {...login}>
+						<button class="navbtn" type="submit">Login</button>
 					</form>
 				</li>
 			{/if}
-			<li><a class="btn btn-primary" href="/guide">Guide</a></li>
+			<li><a class="navbtn" href="/guide">Guide</a></li>
 		</ul>
 	</nav>
-</header>
+</aside>
 
-<main class="px-4 py-20 max-w-240 w-full mx-auto flex-1 min-w-0">
-	{@render children()}
-</main>
+<div class="flex flex-col flex-1 min-w-0 sm:pl-40">
+	<main class="px-4 py-20 max-w-240 w-full mx-auto flex-1 min-w-0">
+		{@render children()}
+	</main>
 
-<footer class="bg-neutral-200 px-8 py-4 text-center">
-	<p class="pb-4">
-		A programme by
-		<a
-			href="https://hackclub.enterprise.slack.com/team/U07JH9LU1NC"
-			target="_blank"
-			rel="noreferrer"
-			>@Heliodex</a
-		>
-		at
-		<a href="https://hackclub.com/" target="_blank" rel="noreferrer"
-			>Hack Club</a
-		>!
-	</p>
+	<footer class="bg-neutral-200 px-8 py-4 text-center">
+		<p class="pb-4">
+			A programme by
+			<a
+				href="https://hackclub.enterprise.slack.com/team/U07JH9LU1NC"
+				target="_blank"
+				rel="noreferrer"
+				>@Heliodex</a
+			>
+			at
+			<a href="https://hackclub.com/" target="_blank" rel="noreferrer"
+				>Hack Club</a
+			>!
+		</p>
 
-	<p>
-		<a
-			href="https://hackclub.com/privacy-and-terms"
-			target="_blank"
-			rel="noreferrer"
-			>Privacy & Terms</a
-		>
-		|
-		<a
-			href="https://forms.hackclub.com/bounty"
-			target="_blank"
-			rel="noreferrer"
-			>Fulfilment bounty</a
-		>
-		|
-		<a href="https://github.com/{repoName}" target="_blank" rel="noreferrer"
-			>Source code</a
-		>
-	</p>
-</footer>
+		<p>
+			<a
+				href="https://hackclub.com/privacy-and-terms"
+				target="_blank"
+				rel="noreferrer"
+				>Privacy & Terms</a
+			>
+			|
+			<a
+				href="https://forms.hackclub.com/bounty"
+				target="_blank"
+				rel="noreferrer"
+				>Fulfilment bounty</a
+			>
+			|
+			<a
+				href="https://github.com/{repoName}"
+				target="_blank"
+				rel="noreferrer"
+				>Source code</a
+			>
+		</p>
+	</footer>
+</div>
