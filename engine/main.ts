131fd1f0974c61a24e30d7208d1dcf3f9aa55b3b
Lewin Kelly <heli@odex.cf>
2026-08-24 15:59:11 -0400

    Format comment

--- engine/main.ts
+++ engine/main.ts
@@ -1,8 +1,7 @@
 import { db, reconnect } from "./db"
 import { start } from "./server"
 
-// The engine connects to the database started by the site. It does not start
-// its own SurrealDB instance. Once connected it runs battles continuously.
+// The engine connects to the database started by the site. It does not start its own SurrealDB instance. Once connected it runs battles continuously.
 await reconnect()
 
 await start(db)
