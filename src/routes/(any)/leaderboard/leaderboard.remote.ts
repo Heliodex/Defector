002835777547cde434b36ef861d5b3704f318fc2
Lewin Kelly <heli@odex.cf>
2026-08-31 18:38:32 -0400

    More leaderboard query optimisation

--- src/routes/(any)/leaderboard/leaderboard.remote.ts
+++ src/routes/(any)/leaderboard/leaderboard.remote.ts
@@ -40,10 +40,9 @@ async function battlesSnapshot(
 async function botsSnapshot(
 	id?: RecordId<"bot", string>
 ): Promise<Pick<LeaderboardData, "bots" | "activeBots">> {
-	const [bots, activeBots] = await db.query<[BotRow[], number]>(
-		leaderboardBotsQuery,
-		{ id }
-	)
+	const [, , , bots, activeBots] = await db.query<
+		[undefined, undefined, undefined, BotRow[], number]
+	>(leaderboardBotsQuery, { id })
 
 	return { bots, activeBots }
 }
