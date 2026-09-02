fb48eff0c36501aefa2bf2a8a2a72147f153bdfe
Lewin Kelly <heli@odex.cf>
2026-09-02 11:46:30 -0400

    Fix leaderboard bot snapshot querying

--- src/routes/(any)/leaderboard/leaderboard.remote.ts
+++ src/routes/(any)/leaderboard/leaderboard.remote.ts
@@ -40,8 +40,16 @@ async function battlesSnapshot(
 async function botsSnapshot(
 	id?: RecordId<"bot", string>
 ): Promise<Pick<LeaderboardData, "bots" | "activeBots">> {
-	const [, , , bots, activeBots] = await db.query<
-		[undefined, undefined, undefined, BotRow[], number]
+	const [, , , , , bots, activeBots] = await db.query<
+		[
+			undefined,
+			undefined,
+			undefined,
+			undefined,
+			undefined,
+			BotRow[],
+			number,
+		]
 	>(leaderboardBotsQuery, { id })
 
 	return { bots, activeBots }
