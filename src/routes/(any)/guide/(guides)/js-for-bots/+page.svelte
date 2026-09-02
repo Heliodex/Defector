3cb1e88e93dfc5b34c0abbb922b42f6287973568
Lewin Kelly <heli@odex.cf>
2026-09-01 21:45:42 -0400

    More detail on JavaScript for bots guide

--- src/routes/(any)/guide/(guides)/js-for-bots/+page.svelte
+++ src/routes/(any)/guide/(guides)/js-for-bots/+page.svelte
@@ -68,11 +68,13 @@ const guideName = "JavaScript for bots"
 
 <p class="pb-4">
 	2 keywords are placed before the definition of the function. The
-	<code>export</code> keyword is used to make the function available to some
-	other code outside of the file &ndash; in this case, that will be the
-	competition engine, which needs to be able to call (run) your bot. The
-	<code>default</code> keyword is used to indicate that this is the
-	primary/only exported function of the file.
+	<code>export</code>
+	keyword is used to make the function available to some other code outside of
+	the file &ndash; in this case, that will be the competition engine, which
+	needs to be able to call (run) your bot. The
+	<code>default</code>
+	keyword is used to indicate that this is the primary/only exported function
+	of the file.
 </p>
 
 <p>
@@ -107,7 +109,8 @@ const guideName = "JavaScript for bots"
 />
 
 <p>
-	Oh yeah, also comments are written starting with <code>//</code> for single-line comments, which extend to the end of the line.<br />
+	Oh yeah, also comments are written starting with <code>//</code> for
+	single-line comments, which extend to the end of the line.<br>
 	We'll stick to the 1st style in these guides for coherence, though you can
 	use whichever you prefer. Next, there are some constant declarations:
 </p>
@@ -133,24 +136,28 @@ const guideName = "JavaScript for bots"
 	indicates an explicit absence of a value. <br>
 	<code>null</code>
 	can often be confused with <code>undefined</code>, which indicates an
-	<em>implicit</em> absence of a value. Not many other programming languages
-	have this distinction. Fun!
+	<em>implicit</em>
+	absence of a value. Not many other programming languages have this
+	distinction. Fun!
 </p>
 
 <p class="pb-4">
 	There are also a few ways to declare constants and variables in JavaScript.
 	These are <code>const</code> and <code>let</code>. The difference is that
-	<code>const</code> does not allow for reassigning a value to the variable,
-	while <code>let</code> does. We've used <code>const</code> here because we
-	don't need to change the values of <code>move</code> or
-	<code>memory</code> after they are assigned.
+	<code>const</code>
+	does not allow for reassigning a value to the variable, while
+	<code>let</code>
+	does. We've used <code>const</code> here because we don't need to change the
+	values of <code>move</code> or
+	<code>memory</code>
+	after they are assigned.
 </p>
 
 <p>
 	Finally, there's a return value, preceded by the
-	<code>return</code> keyword. In JavaScript, functions don't have to return
-	anything, though if we omitted this then our bot wouldn't be able to make a
-	move.
+	<code>return</code>
+	keyword. In JavaScript, functions don't have to return anything, though if
+	we omitted this then our bot wouldn't be able to make a move.
 </p>
 
 <Code
@@ -167,21 +174,23 @@ const guideName = "JavaScript for bots"
 
 <p class="pb-4">
 	Arrays in JavaScript are delimited by square brackets
-	<code>[]</code> (similar to what's sometimes called "lists" or "vectors" in
-	other programming languages). The competition engine expects your bot to
-	return an array with 2 elements: the 1st being the move to make, either
-	<code>"C"</code> for cooperation or <code>"D"</code> for defection, and the
-	2nd being the memory to pass to your bot on its next turn. We add the
-	constants <code>move</code> and <code>memory</code> to the array in their
-	respective places. It doesn't matter what the constants are named, just
-	their order in the returned array.
+	<code>[]</code>
+	(similar to what's sometimes called "lists" or "vectors" in other
+	programming languages). The competition engine expects your bot to return an
+	array with 2 elements: the 1st being the move to make, either
+	<code>"C"</code>
+	for cooperation or <code>"D"</code> for defection, and the 2nd being the
+	memory to pass to your bot on its next turn. We add the constants
+	<code>move</code>
+	and <code>memory</code> to the array in their respective places. It doesn't
+	matter what the constants are named, just their order in the returned array.
 </p>
 
 <p class="pb-4">
 	The competition engine will run this bot, and it will receive the value
-	<code>["C", null]</code> in return. It then keeps track of the move the bot
-	wants to make, and stores the memory value. The bot is then rerun on the
-	next round.
+	<code>["C", null]</code>
+	in return. It then keeps track of the move the bot wants to make, and stores
+	the memory value. The bot is then rerun on the next round.
 </p>
 
 <p>
@@ -209,7 +218,7 @@ const guideName = "JavaScript for bots"
 		if (currentRound == 1) return ["D", null]
 		if (currentRound == 2) return ["C", null]
 
-		if (!memory) {
+		if (memory == null) {
 			const opp0 = history[0].opponent
 			const opp1 = history[1].opponent
 			const opp2 = history[2].opponent
@@ -252,11 +261,15 @@ const guideName = "JavaScript for bots"
 />
 
 <p class="pb-4">
-	The history includes a list of moves by us and our opponent. We'll explore the format of the moves in a little bit, though for now we can find out which round of the battle we're on by looking at the length of this history.
+	The history includes a list of moves by us and our opponent. We'll explore
+	the format of the moves in a little bit, though for now we can find out
+	which round of the battle we're on by looking at the length of this history.
 </p>
 
 <p>
-	We'll start with a simple test: cooperate, defect, cooperate &ndash; and we'll do this by checking the current round and making a move based on that for the first 3 rounds.
+	We'll start with a simple test: cooperate, defect, cooperate &ndash; and
+	we'll do this by checking the current round and making a move based on that
+	for the first 3 rounds.
 </p>
 
 <Code
@@ -274,6 +287,136 @@ const guideName = "JavaScript for bots"
 `}
 />
 
+<p class="pb-4">
+	So here, the <code>currentRound</code> constant holds the length of the
+	battle history, which will be 0 if it's empty. I suppose that makes the
+	round number look like it's off by 1.<br>
+	For the next 3 statements, we check if it's round 0, 1, or 2 using the
+	equality operator <code>==</code>, and early-return the appropriate move and
+	memory. The memory is set to <code>null</code> for now, since we don't need
+	to store any information yet.
+</p>
+
+<p class="pb-4">
+	JavaScript allows for returning at any point during a function, and nothing
+	after a return will be executed. That is, if one of these
+	<code>if</code>
+	statements is true, none of the remaining ones will be checked.
+</p>
+
 <p>
-	So here, the <code>currentRound</code> constant holds the length of the battle history, which will be 0 if it's empty.
+	Next, we'll check our opponent's moves for the first 3 moves
+	<em>they</em>
+	made while we were testing. Each element in the
+	<code>history</code>
+	array is an <b>object</b> with 2 properties:
+	<code>you</code>
+	and <code>opponent</code>. The <code>you</code> property is the move we
+	made, and the <code>opponent</code> property is the move our opponent made.
+</p>
+
+<Code
+	filename="detective.js"
+	code={`
+	export default function bot({ history, memory }) {
+		// ...
+
+~		if (memory == null) {
+~			const opp0 = history[0].opponent
+~			const opp1 = history[1].opponent
+~			const opp2 = history[2].opponent
+
+			// ...
+		}
+
+		return [memory.strategy, memory]
+	}
+`}
+/>
+
+<p class="pb-4">
+	We first check in the <code>if</code> statement whether we've written to
+	memory already, and if we haven't (because it's <code>null</code>), we
+	proceed.<br>
+	Next, we index into the <code>history</code> array by placing an integer in
+	square brackets <code>[number]</code> following the constant name
+	(identifier). JavaScript arrays are 0-indexed, so the first element is 0 and
+	the final element is <code>history.length - 1</code>. For each history item,
+	we take our opponent's move from it using dot notation.
+</p>
+
+<p>
+	Now that we have these values assigned to constants, we'll check what
+	they've done to determine our strategy. This will be simple enough for now:
+	if they cooperated for the beginning 2 rounds and then defected in response
+	to our defection, we'll assume they're a tit-for-tat-type bot and cooperate
+	with them. Otherwise, we'll assume they're either (1) an always-defecting
+	bot that is only worth defecting against, (2) an always-cooperate bot that
+	is worth exploiting, or (3) a random-like bot that is too unpredictable to
+	cooperate with &ndash; in any of these cases, we'll defect against them.
+</p>
+
+<Code
+	filename="detective.js"
+	code={`
+	export default function bot({ history, memory }) {
+		// ...
+
+		if (memory == null) {
+			// ...
+
+			// if the opponent reacted to our defection, we'll cooperate with them
+~			if (opp0 == "C" && opp1 == "C" && opp2 == "D")
+~				memory = { strategy: "C" }
+~			else
+~				memory = { strategy: "D" }
+		}
+
+		return [memory.strategy, memory]
+	}
+`}
+/>
+
+<p class="pb-4">
+	The <code>&&</code> operator here is used as a logical AND operator, so all
+	of the conditions must be met for the entire statement to be true.<br>
+	If we detect a retaliatory defection, we set the memory variable (since it's
+	destructured from the function parameter, it's reassignable by default) to
+	an object with a property <code>strategy</code> set to <code>"C"</code>.
+	Otherwise, we set this strategy property to <code>"D"</code>.<br>
+	Objects are the equivalent of what other languages may call "dictionaries", "maps", or "hashmaps". They store pairs of keys and values. The keys can be text strings, numbers, or other applicable types, and the values can be any type.
+</p>
+
+<p class="pb-4">
+	An additional note on these <code>if</code> statements: the brackets
+	enclosing their bodies are only necessary if a body consists of more than
+	one statement. In the interest of brevity, I've omitted them here everywhere
+	possible.
+</p>
+
+<p>
+	And to complete our bot, we'll return the move we want to make (the same
+	value as the strategy we determined) and the memory object we created. The
+	competition engine will then store this memory and pass it back to our bot
+	on the next round, so we can continue to cooperate or defect based on our
+	opponent's response.
+</p>
+
+<Code
+	filename="detective.js"
+	code={`
+	export default function bot({ history, memory }) {
+		// ...
+
+~		return [memory.strategy, memory]
+	}
+`}
+/>
+
+<p class="pb-4">
+	This bot is designed to cooperate well with bots using tit-for-tat strategies, and to exploit bots that always cooperate. It will also defend itself as much as it can against bots which are random/unpredictable or always defect.
+</p>
+
+<p>
+	Can you figure out what its weaknesses are? Try to build a bot that can beat this one, or try to improve on this version of the detective strategy.
 </p>
