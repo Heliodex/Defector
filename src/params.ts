fb9ddecead7a047f193690ada43517e3db58f8f2
Lewin Kelly <heli@odex.cf>
2026-09-03 16:22:02 -0400

    Fix path traversal issue with regex in admin images endpoint

--- src/params.ts
+++ src/params.ts
@@ -1,7 +1,9 @@
 import { defineParams } from "@sveltejs/kit/params"
 
 const idRegex = /^[0-9a-z]{20}$/
+const sha256Regex = /^[a-f0-9]{64}$/
 
 export const params = defineParams({
 	strid: p => (idRegex.test(p) ? p : undefined),
+	sha256: p => (sha256Regex.test(p) ? p : undefined),
 })
